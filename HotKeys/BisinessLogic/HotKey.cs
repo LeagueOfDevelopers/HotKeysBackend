5f236a5f65fb2206a58fd989947ee7c333ce7982
LavanderSea <tanya.smith1712@gmail.com>
2018-11-27 22:11:58 +0300

    Добавила сущности Программа и Горячая клавиша

---
+++ HotKeys/BisinessLogic/HotKey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BisinessLogic
+{
+    class HotKey
+    {
+        public Guid Id { get; }
+        public string _description { get; }
+        private string[] Combination;
+        public IEnumerable<string> _combination => Combination.AsEnumerable();
+    }
+}
