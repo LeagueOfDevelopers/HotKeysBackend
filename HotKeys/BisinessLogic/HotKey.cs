97a33185087c02726495e27df2bc6e8ce04735ae
LavanderSea <tanya.smith1712@gmail.com>
2018-11-27 22:36:13 +0300

    Add entities HotKey and Program

--- HotKeys/BisinessLogic/HotKey.cs
+++ HotKeys/BisinessLogic/HotKey.cs
@@ -11,6 +11,15 @@ namespace BisinessLogic
         public Guid Id { get; }
         public string _description { get; }
         private string[] Combination;
+
+        public HotKey(Guid id, string description, string[] combination)
+        {
+            Id = id;
+            _description = description;
+            Combination = combination;
+        }
+
         public IEnumerable<string> _combination => Combination.AsEnumerable();
+
     }
 }
