78a2d636c75be1fb1ea915dc7c1c94c80cc88170
LavanderSea <tanya.smith1712@gmail.com>
2018-12-12 16:02:41 +0300

    project restored

---
+++ HotKeys/BisinessLogic/Program_.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BisinessLogic
+{
+    public class Program_
+    {
+        public Program_(Guid id, string name, List<HotKey> hotKeys)
+        {
+            this.id = id;
+            this.name = name;
+            HotKeys = hotKeys;
+        }
+
+        public Guid id { get; }
+        public string name { get; }
+        private List<HotKey> HotKeys;
+        public IEnumerable<HotKey> hotKeys => HotKeys.AsEnumerable();
+        public void AddNewHotKey(HotKey hotKey)
+        {
+            HotKeys.Add(hotKey);
+        }
+        public void ChangeHotKey(Guid hotKeyId, HotKey newHotKey)
+        {
+            var id = HotKeys.FindIndex(hotk => hotk.Id == hotKeyId);
+            HotKeys[id] = newHotKey;
+        }
+    }
+}
